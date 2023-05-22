8c93c996f034e7a34ebe278bbfad21b14f5e0ed6
Carlos Augusto <caarlos.san@gmail.com>
2023-05-22 13:35:04 -0300

    feat: add test_ingredients

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,18 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    farinha = Ingredient("farinha")
+    bacon = Ingredient("bacon")
+
+    assert farinha.name == "farinha"
+    assert farinha.restrictions == {Restriction.GLUTEN}
+    assert farinha.__repr__() == "Ingredient('farinha')"
+
+    assert hash(farinha) != hash(bacon)
+    assert hash(farinha) == hash(farinha)
+    assert farinha.__eq__(farinha)
